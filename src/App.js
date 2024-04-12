1c5fdc1af05b85363d59f0ca8ddca54bf6507663
deep-coder1 <ndktbd@gmail.com>
2024-04-13 05:20:24 +0530

    Create a form that takes user input and displays it in real-time (3)

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react'
 // import One from './One'
-import Two from './Two'
+// import Two from './Two'
+import Three from './Three'
 
 export default function App() {
   return (
     <div>
       {/* <One /> */}
-      <Two />
+      {/* <Two /> */}
+      <Three />
     </div>
   )
 }
