6001bdeb95756560172eba69deabab45e67fdefd
deep-coder1 <ndktbd@gmail.com>
2024-05-17 22:40:56 +0530

    Working on To-Do List using useReducer (22), View List || React JS 30+ Project

--- src/TwentyTwo.js
+++ src/TwentyTwo.js
@@ -1,29 +1,43 @@
-// Create a simple to-do list using useReducer to manage tasks. 
-import { type } from '@testing-library/user-event/dist/type';
-import React, { useReducer } from 'react'
-import { useState } from 'react';
+// Create a simple to-do list using useReducer to manage tasks.
+import { type } from "@testing-library/user-event/dist/type";
+import React, { useReducer } from "react";
+import { useState } from "react";
 
 export default function TwentyTwo() {
-
-    const taskReducer = (state,action) => {
-        switch(action.type){
-            case 'ADD_TASK':
-                return [...state,{id:Date.now(),text:action.payload,completed:false}]
-        }
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_TASK":
+        return [
+          ...state,
+          { id: Date.now(), text: action.payload, completed: false },
+        ];
     }
+  };
 
-    const [state,dispatch] = useReducer(taskReducer,[]);
+  const [tasks, dispatch] = useReducer(taskReducer, []);
 
-    const [taskText,setTaskText] = useState('');
-    const addTask = () => {
-        dispatch({type:'ADD_TASK',payload:taskText});
-    }
+  const [taskText, setTaskText] = useState("");
+  const addTask = () => {
+    dispatch({ type: "ADD_TASK", payload: taskText });
+  };
 
   return (
     <div>
-        <h1>Todo List</h1>
-        <input type='text' value={taskText} onChange={(e)=>setTaskText(e.target.value)} placeholder='Add new Task' />
-        <button onClick={addTask}>Add</button>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={taskText}
+        onChange={(e) => setTaskText(e.target.value)}
+        placeholder="Add new Task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.text}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
