cedd1aec265425c1c14bac1194e3691be6d21cea
deep-coder1 <ndktbd@gmail.com>
2024-05-29 08:05:57 +0530

    Working on Draggable Component (25) || React JS 30+ Project

--- README.md
+++ README.md
@@ -94,4 +94,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 <!-- (22). 10:05 / 13:34 Vid (22) Time End / To-Do List using useReducer (22) || React JS 30+ Project -->
 <!-- (23). 13:13 / 13:13 Vid (23) Time End / Pagination using useReducer (23) || React JS 30+ Project -->
 <!-- (24). 09:09 / 09:09 Vid (24) Time End / User Authentication using useContext (24) || React JS 30+ Project -->
-<!-- (25). 01:09 / 13:10 Vid (25) Time End / Draggable Component (25) || React JS 30+ Project -->
+<!-- (25). 03:20 / 13:10 Vid (25) Time End / Draggable Component (25) || React JS 30+ Project -->
--- src/TwentyFive.js
+++ src/TwentyFive.js
@@ -1,9 +1,32 @@
 // Build a draggable component using useReducer to manage its position.
-import React from 'react'
+import React, { useReducer } from 'react'
+
+function boxReducer(){
+
+}
 
 export default function TwentyFive() {
+
+  const [boxState,dispatch] = useReducer(boxReducer,{left:0,top:0});
+
+  const handleMouseDown = (e) => {
+    //
+  }
+
+  const handleMouseUp = (e) => {
+    //
+  }
+
+  const handleMouseMove = (e) => {
+    console.log(e.clientX)
+  }
+
   return (
-    <div>
+    <div 
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
         Drag Me
     </div>
   )
