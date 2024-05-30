7d82cb5dc51260b6dd1062ec944b46df52d7fb9d
deep-coder1 <ndktbd@gmail.com>
2024-05-30 20:35:42 +0530

    Working on Draggable Component (25) || React JS 30+ Project

--- src/TwentyFive.js
+++ src/TwentyFive.js
@@ -1,24 +1,41 @@
 // Build a draggable component using useReducer to manage its position.
 import React, { useReducer } from 'react'
 
-function boxReducer(){
-
+function boxReducer(state,action){
+  switch(action.type){
+    case 'MOVE'
+    return {
+      ...state,
+      left:action.payload.left,
+      top:action.payload.top
+    };
+    default:
+      return state;
+  }
 }
 
 export default function TwentyFive() {
 
   const [boxState,dispatch] = useReducer(boxReducer,{left:0,top:0});
 
+  let initialX = 0;
+  let initialY = 0;
+
   const handleMouseDown = (e) => {
-    //
+    let isDragging = true;
   }
 
   const handleMouseUp = (e) => {
-    //
+    let isDragging = false;
   }
 
   const handleMouseMove = (e) => {
-    console.log(e.clientX)
+    // console.log(e.clientX)
+    if(isDragging){
+      const left = e.ClientX - initialX;
+      const top = e.ClientY - initialY;
+      dispatch({type:'MOVE',payload:{left,top}});
+    }
   }
 
   return (
